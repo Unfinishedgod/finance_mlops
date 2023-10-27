753afb46af07aa4b21378dddce598e02b00b9b69
Unfinishedgod <shjj08@gmail.com>
2023-10-27 08:02:18 +0000

    modify dir

--- python_file/bitcoin_crawler.py
+++ python_file/bitcoin_crawler.py
@@ -101,6 +101,7 @@ def upload_df(data, file_name, project_id, dataset_id, time_line, today_date1):
         
 ### 날짜 설정
 now = datetime.now()
+now = now + timedelta(days=-1)
 
 today_date1 = now.strftime('%Y%m%d')
 today_date2 = now.strftime('%Y-%m-%d')
