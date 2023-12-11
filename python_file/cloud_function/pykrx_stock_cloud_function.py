f723a163d57e22184f8ad4511d76d98489bf5809
Unfinishedgod <shjj08@gmail.com>
2023-12-11 04:52:00 +0000

    a

--- python_file/cloud_function/pykrx_stock_cloud_function.py
+++ python_file/cloud_function/pykrx_stock_cloud_function.py
@@ -26,11 +26,11 @@ from google.cloud import storage
 
 
 # 경로 변경
-os.chdir('/home/shjj08choi4/finance_mlops')
+# os.chdir('/home/shjj08choi4/finance_mlops')
 
 
 # 서비스 계정 키 JSON 파일 경로
-key_path = glob.glob("key_value/*.json")[0]
+key_path = glob.glob("./*.json")[0]
 
 # Credentials 객체 생성
 credentials = service_account.Credentials.from_service_account_file(key_path)
@@ -45,7 +45,7 @@ storage_client = storage.Client(credentials = credentials,
 bucket_name = 'finance-mlops-proj'    # 서비스 계정 생성한 bucket 이름 입력
 
 # Postgresql 연결
-db_connect_info = pd.read_csv('key_value/db_connect_info.csv')
+db_connect_info = pd.read_csv('./db_connect_info.csv')
 username = db_connect_info['username'][0]
 password = db_connect_info['password'][0]
 host = db_connect_info['host'][0]
@@ -63,21 +63,21 @@ today_date_time_csv = now.strftime("%Y%m%d_%H%M")
 print(f'{today_date2} pykrx_crawler Start')
 
 def upload_df(data, file_name, project_id, dataset_id, time_line, today_date1):
-    if not os.path.exists(f'data_crawler/{file_name}'):
-        os.makedirs(f'data_crawler/{file_name}')
+    if not os.path.exists(f'/tmp/{file_name}'):
+        os.makedirs(f'/tmp/{file_name}')
 
     try:
-        if not os.path.exists(f'data_crawler/{file_name}/{file_name}_{today_date1}.csv'):
-            data.to_csv(f'data_crawler/{file_name}/{file_name}_{today_date1}.csv', index=False, mode='w')
+        if not os.path.exists(f'/tmp/{file_name}/{file_name}_{today_date1}.csv'):
+            data.to_csv(f'/tmp/{file_name}/{file_name}_{today_date1}.csv', index=False, mode='w')
         else:
-            data.to_csv(f'data_crawler/{file_name}/{file_name}_{today_date1}.csv', index=False, mode='a', header=False)
+            data.to_csv(f'/tmp/{file_name}/{file_name}_{today_date1}.csv', index=False, mode='a', header=False)
         print(f'{file_name}_로컬CSV저장_success_{time_line}')
     except:
         print(f'{file_name}_로컬CSV저장_fail_{time_line}')
 
 
     # Google Storage 적재
-    source_file_name = f'data_crawler/{file_name}/{file_name}_{today_date1}.csv'    # GCP에 업로드할 파일 절대경로
+    source_file_name = f'/tmp/{file_name}/{file_name}_{today_date1}.csv'    # GCP에 업로드할 파일 절대경로
     destination_blob_name = f'data_crawler/{file_name}/{file_name}_{today_date1}.csv'    # 업로드할 파일을 GCP에 저장할 때의 이름
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
@@ -181,3 +181,27 @@ time_line = now1.strftime("%Y%m%d_%H:%M:%S")
 upload_df(df_raw, file_name, project_id, dataset_id, time_line, today_date1)
 
 print(f'DIV/BPS/PER/EPS 완료_{time_line}')
+
+
+
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(name)
