46c334fa4633c8cf41961210ebc9043ce345dcfe
Unfinishedgod <shjj08@gmail.com>
2023-12-30 17:43:33 +0000

    a

--- cloud_run_test/pykrx_1220/cloud_run_shell.sh
+++ cloud_run_test/pykrx_1220/cloud_run_shell.sh
@@ -1,6 +1,6 @@
 
 
-count_nm='6'
+count_nm='8'
 
 docker build -t test_image${count_nm}:tag${count_nm} .
 docker tag test_image${count_nm}:tag${count_nm}  asia-northeast3-docker.pkg.dev/owenchoi-404302/cloud-run-test/test_image${count_nm}:tag${count_nm}
