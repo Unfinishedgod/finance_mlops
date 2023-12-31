e8d0ca5cea8b5762f8d005c32c03b6b159edd8f6
Unfinishedgod <shjj08@gmail.com>
2023-12-31 09:59:09 +0000

    a

--- cloud_run_test/cloud_run_git/cloud_run_shell.sh
+++ cloud_run_test/cloud_run_git/cloud_run_shell.sh
@@ -1,6 +1,6 @@
 
 
-count_nm='8'
+count_nm='9'
 
 docker build -t test_image${count_nm}:tag${count_nm} .
 docker tag test_image${count_nm}:tag${count_nm}  asia-northeast3-docker.pkg.dev/owenchoi-404302/cloud-run-test/test_image${count_nm}:tag${count_nm}
--- cloud_run_test/cloud_run_git/main.py
+++ cloud_run_test/cloud_run_git/main.py
@@ -10,33 +10,23 @@ from time import sleep
 
 import os
 import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    """Example Hello World route."""
+    name = os.environ.get("NAME", "World")
+    
+    usdkrw = fdr.DataReader('USD/KRW', '2023-01-01', '2023-12-01') # 달러 원화
+    usdkrw = usdkrw.reset_index()
+    print(len(usdkrw))
+    
+    usd_nm = len(usdkrw)
 
 
-usdkrw = fdr.DataReader('USD/KRW', '2023-01-01', '2023-12-01') # 달러 원화
-usdkrw = usdkrw.reset_index()
-print(len(usdkrw))
+    return f"Hello {name} {usd_nm}!"
 
-@functions_framework.http
-def hello_http(request):
-
-
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
-
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Hello {}!'.format(name)
-
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
