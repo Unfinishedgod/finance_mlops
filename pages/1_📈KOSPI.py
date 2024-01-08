9f3d14f4b2d8f7328df55c9e71107cd0226f7a48
Unfinishedgod <shjj08@gmail.com>
2024-01-08 23:21:13 +0900

    a

--- pages/1_📈KOSPI.py
+++ pages/1_📈KOSPI.py
@@ -116,7 +116,7 @@ with col2:
   
   df = kor_index_ohlcv_cleaning.groupby(['index_code','index_code_nm'])['close'].apply(list).reset_index()
   
-  index_list_df = index_code_master[index_code_master['ticker'] == ticker_nm_option].reset_index(drop = True)
+  index_list_df = index_code_master[index_code_master['ticker'] == str(ticker_nm_option)].reset_index(drop = True)
   
   df_2 = df[df['index_code'].isin(index_list_df['index_code'])].reset_index(drop = True)
   
