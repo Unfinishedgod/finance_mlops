a2af7bc5283a6ec397a32058f876121ecb5d4709
Unfinishedgod <shjj08@gmail.com>
2024-01-08 23:18:38 +0900

    a

--- pages/1_📈KOSPI.py
+++ pages/1_📈KOSPI.py
@@ -119,22 +119,20 @@ with col2:
   
   df_2 = df[df['index_code'].isin(index_list_df['index_code'])].reset_index(drop = True)
   
-
-  
-  st.dataframe(
-      df_2,
-      column_config={
-          "index_code": "App name",
-          "index_name": "App index_name",
-          "url": st.column_config.LinkColumn("App URL"),
-          "close": st.column_config.LineChartColumn(
-              "Views (past 30 days)", 
-          ),
-      },
-      hide_index=True,
-  )
+  # st.dataframe(
+  #     df_2,
+  #     column_config={
+  #         "index_code": "App name",
+  #         "index_name": "App index_name",
+  #         "url": st.column_config.LinkColumn("App URL"),
+  #         "close": st.column_config.LineChartColumn(
+  #             "Views (past 30 days)", 
+  #         ),
+  #     },
+  #     hide_index=True,
+  # )
   
   
   ################################################################################################
   ################################################################################################
-  st.dataframe(index_code_master)
+st.dataframe(index_code_master)
