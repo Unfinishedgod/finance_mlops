11eecce7e782e6d5e11c7844f0a0d5f6c7f23d04
Unfinishedgod <shjj08@gmail.com>
2024-01-16 00:17:58 +0900

    a

--- start_shell.sh
+++ start_shell.sh
@@ -1,6 +1,6 @@
 #!./bin/bash
 
-python_dir= "/home/shjj08choi4/anaconda3/bin/python3"
+python_dir="/home/shjj08choi4/anaconda3/bin/python3"
 file_dir="/home/shjj08choi4/finance_mlops/python_file"
 
 # # 현재 시간이 오후 6시 이후이면
