b000476c8707c22a8d5229972570b358b3657e0e
Unfinishedgod <shjj08@gmail.com>
2024-01-16 23:18:50 +0900

    a

--- 📊Finance.py
+++ 📊Finance.py
@@ -255,4 +255,10 @@ col2.dataframe(
 
 
 
-# st.dataframe(buy_sell_count_kospi)
+st.sidebar.markdown(
+  """
+    ---
+    - Email : shjj08@gmail.com
+    - Blog : https://unfinishedgod.netlify.app/
+"""
+)
