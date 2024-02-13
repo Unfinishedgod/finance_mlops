92a756f85ffcd2815604b7d9e1744d97b9c49e06
Unfinishedgod <shjj08@gmail.com>
2024-02-13 20:35:20 +0900

    modify index

--- python_file/cleaning/cleaning_index.py
+++ python_file/cleaning/cleaning_index.py
@@ -66,8 +66,8 @@ today_date1 = now.strftime('%Y%m%d')
 today_date2 = now.strftime('%Y-%m-%d')
 today_date_time_csv = now.strftime("%Y%m%d_%H%M")
 
-today_date1 = '2024010'
-today_date2 = '2023-01-07'
+# today_date1 = '2024010'
+# today_date2 = '2023-01-07'
 
 today_date1 = 'reset'
 
