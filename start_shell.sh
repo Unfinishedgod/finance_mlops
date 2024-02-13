d63285f96fbc604f889c7ec60f3d1695e9007791
Unfinishedgod <shjj08@gmail.com>
2024-02-13 20:24:22 +0900

    a

--- start_shell.sh
+++ start_shell.sh
@@ -6,11 +6,11 @@ file_dir="/home/shjj08choi4/finance_mlops/python_file"
 # 현재 시간이 오후 6시 이후이면 오후 7시 이전이면
 # if [ $(date +%H) -ge 18 -a $(date +%H) -lt 19 ]; then
 
-echo "수집: pykrx 수집 시작"
-${python_dir} ${file_dir}/pykrx_crawler.py
+# echo "수집: pykrx 수집 시작"
+# ${python_dir} ${file_dir}/pykrx_crawler.py
 
-echo "수집: pykrx index 수집 시작"
-${python_dir} ${file_dir}/pykrx_crawler_index.py
+# echo "수집: pykrx index 수집 시작"
+# ${python_dir} ${file_dir}/pykrx_crawler_index.py
 
 echo "전처리: pykrx cleaning 코스피 시작"
 ${python_dir} ${file_dir}/cleaning/cleaning_stock_kospi.py
